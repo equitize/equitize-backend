f8ad4bdbd84c7481908d0c1d76ae5262932ac67c
shiyingt <teyshiying1999@gmail.com>
2021-05-23 00:31:28 +0800

    get tables by company name and email

--- api/app/routes/startup.routes.js
+++ api/app/routes/startup.routes.js
@@ -6,12 +6,6 @@ module.exports = app => {
     // Create a new Startup
     router.post("/", startup.create);
   
-    // Retrieve all Startup
-    router.get("/", startup.findAll);
-  
-    // Retrieve a single Startup with id
-    router.get("/:id", startup.findOne);
-  
     // Update a Startup with id
     router.put("/:id", startup.update);
   
@@ -20,6 +14,27 @@ module.exports = app => {
   
     // Delete all Startup
     router.delete("/", startup.deleteAll);
+
+    ////////////////
+    //custom routes
+    ///////////////
+
+    // Retrieve Startup by name
+    router.get("/company_name/:company_name", startup.findViaName);
+    //gg need to read up on conventions of naming but for now,
+    //http://localhost:8080/api/startup/company_name/erm?company_name=equitize
+
+    // Retrieve Startup by email
+    router.get("/email/:email", startup.findViaEmail);
+    //http://localhost:8080/api/startup/email/erm?=banana@mail.xyz
+
+    ///////////////////
+    // Retrieve a single Startup with id
+    router.get("/:id", startup.findOne);
+
+    // Retrieve all Startup
+    router.get("/", startup.findAll);
+
   
     app.use('/api/startup', router);
   };
