332aba8a1a2e1f5d0fa2e53f8c7a8b6e6b67cb52
tinkitwong <tinkitwong@gmail.com>
2021-05-26 19:46:03 +0800

    Cleaned Dir Hierarchy

---
+++ auth0/utils/auth0_config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    auth0_config: {
+        roles: {
+            admin: "rol_1Lw0REn5fa5c5tLr",
+            retail: "rol_N3PSCpEkeuh9mqdo",
+            startup: "rol_vIfCrCeBWu6QlwDR",
+        }
+    }
+}
