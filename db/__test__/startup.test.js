0ca5192f1a7abeca4b69d3b86ba4c1452a690819
hkmac <tonghuikang@outlook.com>
2021-06-01 13:45:34 +0800

    jest test sample

--- db/__test__/startup.test.js
+++ db/__test__/startup.test.js
@@ -1,6 +1,6 @@
 const app = require("../../app")
 const supertest = require('supertest')
-const  db  = require("../models/index")
+const db  = require("../models/index")
 require('mysql2/node_modules/iconv-lite').encodingExists('cesu8');
 
 
@@ -12,30 +12,79 @@ it('Testing to see if Jest works', () => {
 // this is an eg. of one test suite. 
 describe('Startup Creation', () => {
   let thisDb = db
-  beforeEach(async () => {
+  beforeAll(async () => {
     await thisDb.sequelize.sync({ force: true })
   });
 
+  const company_name = 'equitize'
+  const email_address = `company-${company_name}@email.com`
+  const company_password = 'password'
+  const company_name_alt = 'tesla_motors'
+  const email_address_alt = `company-${company_name_alt}@email.com`
+  const company_password_alt = 'password'
+
   it('Testing startup creation API [/api/db/startup]', async() => {
-        const company_name = 'equitize'
-        const email_address = `company-${company_name}@email.com`
-        const company_password = 'password'
-        const requestBody ={
-          company_name:company_name,
-          email_address:email_address,
-          company_password:company_password
-        }
-        const res = await supertest(app)
-                              .post("/api/db/startup")
-                              .send(requestBody)
-        expect(res.statusCode).toBe(200)
+    let requestBody = {
+      company_name:company_name,
+      email_address:email_address,
+      company_password:company_password
+    }
+    let res = await supertest(app)
+                          .post("/api/db/startup")
+                          .send(requestBody)
+    expect(res.statusCode).toBe(200)
+    const company_id = res.body.id    
   });
+
+  it('Testing startup creation API [/api/db/startup] with missing info', async() => {
+    let requestBody = {
+      company_name:company_name,
+      email_address:email_address,
+      // company_password:company_password  // info missed
+    }
+    let res = await supertest(app)
+                          .post("/api/db/startup")
+                          .send(requestBody)
+    expect(res.statusCode).toBe(400)
+  });
+
+  it('Testing startup creation API [/api/db/startup] with duplicate info', async() => {
+    let requestBody ={
+      company_name:company_name,  // duplicate info
+      email_address:email_address,  // duplicate info
+      company_password:company_password
+    }
+    let res = await supertest(app)
+                          .post("/api/db/startup")
+                          .send(requestBody)
+    expect(res.statusCode).toBe(500)
+  });
+
+  it('Testing startup creation API [/api/db/startup] with different info', async() => {
+    let requestBody = {
+      company_name:company_name_alt,
+      email_address:email_address_alt,
+      company_password:company_password_alt
+    }
+    let res = await supertest(app)
+                          .post("/api/db/startup")
+                          .send(requestBody)
+    expect(res.statusCode).toBe(200)
+  });
+
+  it('Testing get all', async() => {
+    requestBody = {}
+    res = await supertest(app)
+                          .get("/api/db/startup")
+                          .send(requestBody)
+    expect(res.body.length).toBe(2)
+    expect(res.statusCode).toBe(200)
+  });
+
   afterAll(async () => {
     await thisDb.sequelize.close()
-  });
-
+  })
 })
-
 
 // describe("test create startup with db", () => {
 //   // Set the db object to a variable which can be accessed throughout the whole test file
