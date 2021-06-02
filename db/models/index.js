2bc25de1cd82d130da1b9f5d939979a96b5731a0
tinkitwong <tinkitwong@gmail.com>
2021-06-03 01:25:03 +0800

    Minor Changes

--- db/models/index.js
+++ db/models/index.js
@@ -34,6 +34,6 @@ db.retailInvestors = require("./retailInvestors.model.js")(sequelize, Sequelize)
 db.campaign = require("./campaign.model.js")(sequelize, Sequelize);
 db.junctionTable = require("./junctionTable.model.js")(sequelize, Sequelize);
 db.commercialChampion = require("./commercialChampion.model.js")(sequelize, Sequelize);
-// db.milestone = require("./milestone.model.js")(sequelize, Sequelize);
+db.milestone = require("./milestone.model.js")(sequelize, Sequelize);
 
 module.exports = db;
