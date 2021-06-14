f80b623707d0e5eab83dae50efae9dd4f5a1726e
tinkitwong <tinkitwong@gmail.com>
2021-06-14 20:17:51 +0800

    Updated db.config.js

---
+++ auth0/controllers/jwt.controller.js
@@ -0,0 +1,7 @@
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+const jwtAuthz = require("express-jwt-authz");
+
+module.exports = {
+    
+}
--- db/config/db.config.js
+++ db/config/db.config.js
@@ -6,7 +6,7 @@ if (process.env.GITHUB_ACTIONS == 'true') {
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "Happymon10!", //insert your own password above, but do not commit
+    PASSWORD: password, //insert your own password above, but do not commit
     DB: "testdb", //make sure database with this name is instantiated on your mysql
     dialect: "mysql",
     pool: {
