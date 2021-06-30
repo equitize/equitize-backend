e7daf09743a9f8765a0f100f300e4d2c9cfa00bd
tinkitwong <tinkitwong@gmail.com>
2021-06-30 16:40:47 +0800

    Cleaned Logs

--- utils/log/logger.js
+++ utils/log/logger.js
@@ -5,7 +5,7 @@ var rfs = require('rotating-file-stream'); // version 2.x
 // create a rotating write stream
 var retailInvAccessLogStream = rfs.createStream('access.log', {
     interval: '30s', // rotate 30s
-    path: path.join(__dirname, 'log/retailInvestor')
+    path: path.join(__dirname, 'logs/retailInvestor')
 })
 
 // custom tokens
