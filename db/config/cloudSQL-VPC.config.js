71f42059585076380dc0b1e821c4722d76736908
Cre8tion <ljl1997@gmail.com>
2021-07-21 19:29:46 +0800

    Testing environment variables

--- db/config/cloudSQL-VPC.config.js
+++ db/config/cloudSQL-VPC.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     HOST: "10.252.128.3",
     USER: "equitize-backend",
-    PASSWORD: "9fM7'ZRn9B,DFB7r", //insert your own password here
+    PASSWORD: process.env.SQL_PASSWORD, //insert your own password here
     DB: "equitize_cloudsql", //make sure database with this name is instantiated on your mysql
     dialect: "mysql",
     pool: {
