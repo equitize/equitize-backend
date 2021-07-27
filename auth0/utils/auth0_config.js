64feb4d129e26013c4b9616d4174a933c1bc0133
tinkitwong <tinkitwong@gmail.com>
2021-07-27 13:24:07 +0800

    Fixed Admin Token API

--- auth0/utils/auth0_config.js
+++ auth0/utils/auth0_config.js
@@ -14,6 +14,6 @@ module.exports = {
         superPerms: {
             admin: "admin:all"
         },
-        adminUID: "auth0|60fd6d77cc74e6006a0acf8d"
+        adminUID: "auth0|60ff98018b2d6b00692b9f7c"
     }
 }
